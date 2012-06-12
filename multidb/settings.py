48c0b3fe160a6a33ef487fccec9bac19b42aacc3
Allan Lei <allanlei@helveticode.com>
2012-06-12 09:40:24 -0700

    add WIP

--- multidb/settings.py
+++ multidb/settings.py
@@ -2,6 +2,13 @@ from django.conf import settings
 
 
 MULTIDB_MAX_CONNECTIONS = int(getattr(settings, 'MULTIDB_MAX_CONNECTIONS', 20))
+MULTIDB_MAX_DB_ALIASES = int(getattr(settings, 'MULTIDB_MAX_DB_ALIASES', MULTIDB_MAX_CONNECTIONS))
+
+
+
+
+
+MULTIDB_MAX_CONNECTIONS = int(getattr(settings, 'MULTIDB_MAX_CONNECTIONS', 20))
 MULTIDB_MAX_CONNECTIONS_MAPPING = dict(getattr(settings, 'MULTIDB_MAX_CONNECTIONS_MAPPING', {}))
 MULTIDB_CLEANUP_FUNCTION = getattr(settings, 'MULTIDB_CLEANUP_FUNCTION', 'multidb.utils.inorder_cleanup')
 MULTIDB_EXCLUDE_FROM_CLEANUP = tuple(getattr(settings, 'MULTIDB_EXCLUDE_FROM_CLEANUP', ('default', )))
